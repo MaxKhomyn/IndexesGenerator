fac8b45602ae72a04b0a4afd5cf75442e445963c
maksym.khomyn <maksym.khomyn@justanswer.com>
2022-09-03 15:02:13 +0300

    Create factory for algorithms

---
+++ IndexesGenerator/Algorithm/AlgorithmTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexesGenerator.Algorithm
+{
+    internal class AlgorithmTypes
+    {
+        protected readonly Dictionary<AlgorithmType, Func<IndexesAlgorithm>> Types;
+
+        public AlgorithmTypes()
+        {
+            Types = new Dictionary<AlgorithmType, Func<IndexesAlgorithm>>();
+
+            Types.Add(AlgorithmType.Squere, GetSquareAlgorithm);
+            Types.Add(AlgorithmType.Snake, GetSnakeAlgorithm);
+        }
+
+        private IndexesAlgorithm GetSnakeAlgorithm() => new SnakeAlgorithm();
+
+        private IndexesAlgorithm GetSquareAlgorithm() => new SquareAlgorithm();
+
+        public IndexesAlgorithm GetAlgorithm(AlgorithmType type)
+        {
+            try
+            {
+                return Types[type]();
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new ArgumentException("Could not define Algorithm Type");
+            }
+        }
+    }
+}
--- IndexesGenerator/Generator.cs
+++ IndexesGenerator/Generator.cs
@@ -7,34 +7,6 @@ namespace IndexesGenerator
 {
     public class Generator
     {
-        protected readonly Dictionary<AlgorithmType, Func<IndexesAlgorithm>> AlgorithmTypes;
-
-        public Generator()
-        {
-            AlgorithmTypes = new Dictionary<AlgorithmType, Func<IndexesAlgorithm>>();
-
-            AlgorithmTypes.Add(AlgorithmType.Squere, GetSquareAlgorithm);
-            AlgorithmTypes.Add(AlgorithmType.Snake, GetSnakeAlgorithm);
-        }
-
-        private IndexesAlgorithm GetSnakeAlgorithm() => new SnakeAlgorithm();
-
-        private IndexesAlgorithm GetSquareAlgorithm() => new SquareAlgorithm();
-
-        private IEnumerable<Position> GenerateIndexes(GeneratorConfig config)
-        {
-            try
-            {
-                var algorithmType = AlgorithmTypes[config.Type];
-
-                return algorithmType().Generate(config);
-            }
-            catch (KeyNotFoundException)
-            {
-                throw new ArgumentException("Could not define Algorithm Type");
-            }
-        }
-
         public IEnumerable<Position> Generate(GeneratorConfig config)
         {
             if (config is null)
@@ -42,7 +14,11 @@ namespace IndexesGenerator
                 throw new ArgumentNullException("config - can not be null");
             }
 
-            var enumerable = GenerateIndexes(config);
+            var algorithmTypes = new AlgorithmTypes();
+
+            var enumerable = algorithmTypes
+                .GetAlgorithm(config.Type)
+                .Generate(config);
 
             var isReverse = config.Direction == AlgorithmDirection.Reverse;
 
